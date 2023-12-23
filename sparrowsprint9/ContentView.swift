2be73e2fb8547cc6c8379e8ffd58040f65b72360
Artem <adelelain@gmail.com>
2023-12-23 18:55:18 +0300

    Fix gradient

--- sparrowsprint9/ContentView.swift
+++ sparrowsprint9/ContentView.swift
@@ -8,61 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var dragOffset: CGSize
+    @State var dragOffset: CGSize = .zero
+    @State var gradientStartPosition: UnitPoint = .zero
+    @State var canvasCenter: CGPoint = .zero
+    
     
     var body: some View {
         
-        Rectangle()
-            .fill(.linearGradient(colors: [Color.red, Color.yellow], startPoint: .top, endPoint: .bottom))
-        
-            .mask {
-                
-                Canvas { context, size in
-                    context.addFilter(.alphaThreshold(min: 0.5, color: .red))
-                    context.addFilter(.blur(radius: 30))
+        GeometryReader { proxy in
+            
+            Rectangle()
+                .fill(.radialGradient(colors: [Color.yellow, Color.red], center: .center, startRadius: 60, endRadius: 120))
+                .mask {
                     
-                    context.drawLayer { ctx in
-                        for index in [1, 2] {
-                            
-                            if let resolvedView = context.resolveSymbol(id: index) {
-                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                    Canvas { context, size in
+                        context.addFilter(.alphaThreshold(min: 0.5))
+                        context.addFilter(.blur(radius: 20))
+                        
+                        context.drawLayer { ctx in
+                            for index in [1, 2] {
+                                
+                                if let resolvedView = context.resolveSymbol(id: index) {
+                                    let drawPosition = CGPoint(x: size.width / 2,
+                                                               y: size.height / 2)
+                                    ctx.draw(resolvedView, at: drawPosition)
+                                    if (index == 2) {
+                                        canvasCenter = drawPosition
+                                    }
+                                }
+                                
                             }
-                            
                         }
+                    }  symbols: {
+                        yellowBall().tag(1)
+                        redBall(offset: dragOffset)
+                            .tag(2)
                     }
-                }  symbols: {
-                    yellowBall().tag(1)
-                    redBall(offset: dragOffset).tag(2)
-                }
-                
-            } .gesture(DragGesture()
-                .onChanged { value in
-                    dragOffset = value.translation
-                }
-                .onEnded { _ in
-                    withAnimation {
-                        dragOffset = .zero
+                    
+                } .gesture(DragGesture()
+                    .onChanged { value in
+                        dragOffset = value.translation
                     }
-                })
-        
+                    .onEnded { _ in
+                        withAnimation {
+                            dragOffset = .zero
+                        }
+                    })
+            
+        }
     }
     
     func yellowBall() -> some View {
         Circle()
-            .fill(.yellow)
             .frame(width: 150, height: 150)
     }
     
     func redBall(offset: CGSize = .zero) -> some View {
-        Circle()
-            .fill(.red)
-            .frame(width: 150, height: 150)
-            .offset(offset)
+        ZStack {
+            
+            
+            Circle()
+                .frame(width: 150, height: 150)
+                .offset(offset)
+            
+            
+        }
+    }
+    
+    func cloudIcon() -> some View {
+        
+        Image(systemName: "cloud.sun.rain.fill")
+            .font(.title)
+            .foregroundColor(.white)
+        
     }
 }
 
 
 
 #Preview {
-    ContentView(dragOffset: CGSize.zero)
+    ContentView(dragOffset: CGSize.zero, gradientStartPosition: .zero)
 }
