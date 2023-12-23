767a6c077f3f57ac34870ad8ca88490415395d98
Artem <adelelain@gmail.com>
2023-12-23 15:08:17 +0300

    Add gradient background

--- sparrowsprint9/ContentView.swift
+++ sparrowsprint9/ContentView.swift
@@ -12,32 +12,39 @@ struct ContentView: View {
     
     var body: some View {
         
-        Canvas { context, size in
-            context.addFilter(.alphaThreshold(min: 0.5, color: .red))
-            context.addFilter(.blur(radius: 30))
-            
-            context.drawLayer { ctx in
-                for index in [1, 2] {
+        Rectangle()
+            .fill(.linearGradient(colors: [Color.red, Color.yellow], startPoint: .top, endPoint: .bottom))
+        
+            .mask {
+                
+                Canvas { context, size in
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .red))
+                    context.addFilter(.blur(radius: 30))
                     
-                    if let resolvedView = context.resolveSymbol(id: index) {
-                        ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                    context.drawLayer { ctx in
+                        for index in [1, 2] {
+                            
+                            if let resolvedView = context.resolveSymbol(id: index) {
+                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                            }
+                            
+                        }
                     }
-                    
+                }  symbols: {
+                    yellowBall().tag(1)
+                    redBall(offset: dragOffset).tag(2)
                 }
-            }
-        }  symbols: {
-            yellowBall().tag(1)
-            redBall(offset: dragOffset).tag(2)
-        }
-        .gesture(DragGesture()
-            .onChanged { value in
-                dragOffset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    dragOffset = .zero
+                
+            } .gesture(DragGesture()
+                .onChanged { value in
+                    dragOffset = value.translation
                 }
-            })
+                .onEnded { _ in
+                    withAnimation {
+                        dragOffset = .zero
+                    }
+                })
+        
     }
     
     func yellowBall() -> some View {
